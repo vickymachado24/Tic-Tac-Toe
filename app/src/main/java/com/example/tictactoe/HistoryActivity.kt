a66da9ddb1be4872581006a6430ebeee92c703c5
jkeshava <jkeshava@asu.edu>
2024-10-07 12:02:55 -0700

    Resolving conflict with HistoryActivity.kt

--- app/src/main/java/com/example/tictactoe/HistoryActivity.kt
+++ app/src/main/java/com/example/tictactoe/HistoryActivity.kt
@@ -1,49 +1,5 @@
 package com.example.tictactoe
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-
-class HistoryActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text("Tic Tac Toe")
-                },
-                navigationIcon = {
-                    IconButton(onClick = { finish()
-                    }) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                },
-            )
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {}
-        }
-    }
-}
-
 import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
