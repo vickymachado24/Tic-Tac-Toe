81e94a57188714ec1ef2cbdd59e009c6788988f3
jkeshava <jkeshava@asu.edu>
2024-10-07 11:44:12 -0700

    Update MainActivity.kt

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-<<<<<<< Updated upstream
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,17 +18,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-=======
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
->>>>>>> Stashed changes
 import com.example.tictactoe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,7 +27,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TicTacToeTheme {
-<<<<<<< Updated upstream
                 CenterAlignedTopAppBar(
                     title = {
                         Text("Tic Tac Toe")
@@ -52,7 +39,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Button(
                         onClick = {
-                             val intent = Intent(this@MainActivity, GameActivity::class.java)
+                             val intent = Intent(this@MainActivity, SettingsActivity::class.java)
                              startActivity(intent)
                         },
                         content = {
@@ -77,51 +64,8 @@ class MainActivity : ComponentActivity() {
                             Text("Past Games")
                         },
                     )
-                    Button(
-                        onClick = {
-                             val intent = Intent(this@MainActivity, SettingsActivity::class.java)
-                             startActivity(intent)
-                        },
-                        content = {
-                            Text("Settings")
-                        },
-=======
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(
-                        modifier = Modifier.padding(innerPadding)
->>>>>>> Stashed changes
-                    )
                 }
             }
         }
     }
-<<<<<<< Updated upstream
 }
-=======
-}
-
-@Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    val context = LocalContext.current // Obtain the current context
-
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center // Aligns content to the center
-    ) {
-        Button(onClick = {
-            // Intent to start the com.example.tictactoe.HistoryActivity
-            context.startActivity(Intent(context, HistoryActivity::class.java))
-        }) {
-            Text(text = "History")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainScreenPreview() {
-    TicTacToeTheme {
-        MainScreen()
-    }
-}
->>>>>>> Stashed changes
